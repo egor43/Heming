cd141bdc06e0d76eae3d089ff74bbeccecd24a44
egor43 <egor43.rus@mail.ru>
2016-11-26 16:06:06 +0300

    Перенес проект, который начал в универе

---
+++ Hemming/Hemming/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hemming
+{
+    class Program
+    {
+        static int[] ConvertToBinaryArray(string message)
+        {
+            int[] res = new int[message.Length];
+            for (int i = 0; i < message.Length; i++)
+            {
+                res[i]=Convert.ToInt32(((Convert.ToString(message[i], 2))));
+            }
+            return res;
+        }
+
+        static int GetEncodedCharacter(int ch)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            string mess = Console.ReadLine();
+            foreach (var v in ConvertToBinaryArray(mess))
+            {
+                Console.WriteLine(v);
+            }
+            Console.ReadLine();
+        }
+    }
+}
